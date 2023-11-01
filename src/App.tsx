76a717e31f208243c2a3553acba5389e9d74bbd7
Seongki Kim <moztiq@gmail.com>
2023-11-01 19:02:34 +0900

    gestures

--- src/App.tsx
+++ src/App.tsx
@@ -10,57 +10,24 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  box-shadow:
-    0 2px 3px rgba(0, 0, 0, 0.1),
-    0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-`;
-
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 50%;
   box-shadow:
     0 2px 3px rgba(0, 0, 0, 0.1),
     0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  start: { opacity: 1, scale: 0 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariants = {
-  start: { y: 20, opacity: 0 },
-  end: {
-    y: 0,
-    opacity: 1,
-  },
+  hover: { scale: 1.5, rotate: 90 },
+  click: { borderRadius: '100px', scale: 1 },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box variants={boxVariants} whileHover="hover" whileTap="click"></Box>
     </Wrapper>
   );
 }
