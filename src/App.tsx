2777a237311d39fe55f53c151e97e6ce69574c99
Seongki Kim <moztiq@gmail.com>
2023-11-01 18:51:40 +0900

    variants part two

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,55 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow:
+    0 2px 3px rgba(0, 0, 0, 0.1),
+    0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const Circle = styled(motion.div)`
   background-color: white;
-  border-radius: 10px;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 50%;
   box-shadow:
     0 2px 3px rgba(0, 0, 0, 0.1),
     0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-function App() {
-  const myVars = {
-    start: { scale: 0 },
-    end: {
-      scale: 1,
-      rotate: 180,
-      transition: { delay: 0.5, type: 'spring', bounce: 0.5 },
+const boxVariants = {
+  start: { opacity: 1, scale: 0 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
     },
-  };
+  },
+};
+
+const circleVariants = {
+  start: { y: 20, opacity: 0 },
+  end: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
