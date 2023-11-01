1cdb3e9fac88101456e1406e92d5127cb3cd95a2
Seongki Kim <moztiq@gmail.com>
2023-11-01 16:53:07 +0900

    reset project

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,28 @@
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import styled from 'styled-components';
-import { useRecoilState } from 'recoil';
-import { toDoState } from './atoms';
-import Board from './components/Board';
 
 const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  max-width: 720px;
-  width: 100%;
-  margin: 0 auto;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 `;
 
-const Boards = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 100%;
-  gap: 10px;
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow:
+    0 2px 3px rgba(0, 0, 0, 0.1),
+    0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-  const [toDos, setTodos] = useRecoilState(toDoState);
-
-  const onDragEnd = (info: DropResult) => {
-    const { source, destination } = info;
-    if (!destination) return;
-
-    if (destination.droppableId === source.droppableId) {
-      // same board movement
-      setTodos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    } else {
-      setTodos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board key={boardId} boardId={boardId} toDos={toDos[boardId]} />
-          ))}
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -59,9 +59,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-weight: 300;
     font-family: 'Roboto', sans-serif;
-    background-color: ${(props) => props.theme.bgColor};
     color:black;
     line-height: 1.2;
+    background:linear-gradient(135deg,#e09,#d0e);
   }
   a {
     text-decoration: none;
