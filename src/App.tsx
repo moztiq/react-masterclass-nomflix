1d65ade8ee5b7c72b9e6e5a17cc5ff665263b865
Seongki Kim <moztiq@gmail.com>
2023-11-01 17:25:27 +0900

    basic animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,7 +9,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -21,7 +22,11 @@ const Box = styled.div`
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 90 }}
+        transition={{ delay: 0.5, type: 'spring', bounce: 0.5 }}
+      />
     </Wrapper>
   );
 }
