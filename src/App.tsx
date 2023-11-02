f4f4fa900684e135a5ee1e85198a1540816564cb
Seongki Kim <moztiq@gmail.com>
2023-11-02 17:20:16 +0900

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #e09, #d0e);
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -16,50 +17,72 @@ const Box = styled(motion.div)`
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   box-shadow:
     0 2px 3px rgba(0, 0, 0, 0.1),
     0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  position: absolute;
+  top: 100px;
 `;
 
-function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => {
-    setShowing((prev) => !prev);
-  };
-
-  const boxVariants = {
-    hidden: {
+const box = {
+  entry: (back: boolean) => {
+    console.log('entry back', back);
+    return {
+      x: back ? -500 : 500,
       opacity: 0,
-      scale: 0,
-    },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      rotateZ: 360,
+    };
+  },
+  exit: (back: boolean) => {
+    console.log('exit back', back);
+    return {
+      x: back ? 500 : -500,
+      opacity: 0,
       transition: {
-        duration: 0.3,
+        duration: 0.5,
       },
+    };
+  },
+  center: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
     },
-    exit: {
-      opacity: 0,
-      y: 20,
-    },
+  },
+};
+function App() {
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   };
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  console.log('re-rendered');
+
   return (
     <Wrapper>
-      <AnimatePresence>
-        {showing && (
-          <Box
-            variants={boxVariants}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-          />
-        )}
+      <AnimatePresence custom={back}>
+        <Box
+          key={visible}
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
-      <button onClick={toggleShowing}>Click</button>
+      <button onClick={prevPlease}>prev</button>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
